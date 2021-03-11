a4c9b3caaf2fb35acd76482ffcab17549f033886
debugged-hosting <mbuboltz@debugged-hosting.com>
2021-03-11 07:07:35 -0600

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -42,9 +42,9 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.indi.innerLoopGainBP = [0, 20, 40]
     ret.lateralTuning.indi.innerLoopGainV = [1.5, 2.5, 2.5]
     ret.lateralTuning.indi.outerLoopGainBP = [0, 20, 40]
-    ret.lateralTuning.indi.outerLoopGainV = [2.5, 3.5, 3.75]
+    ret.lateralTuning.indi.outerLoopGainV = [2.5, 3.5, 4.0]
     ret.lateralTuning.indi.timeConstantBP = [0, 20, 40]
-    ret.lateralTuning.indi.timeConstantV = [0.5, 0.8, 0.8]
+    ret.lateralTuning.indi.timeConstantV = [0.5, 0.8, 1.0]
     ret.lateralTuning.indi.actuatorEffectivenessBP = [0, 10, 20]
     ret.lateralTuning.indi.actuatorEffectivenessV = [2.0, 4.0, 4.5]
     ret.steerActuatorDelay = 0.02
